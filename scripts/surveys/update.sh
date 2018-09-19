7292f42a67529485b8d62c2e3bd487ae060af4f3
Jen Fox <jenniferfox4@gmail.com>
2018-09-19 14:01:19 -0400

    Curl script for Update Survey route - updated curl script

--- scripts/surveys/update.sh
+++ scripts/surveys/update.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 API="http://localhost:4741"
-URL_PATH="/examples"
+URL_PATH="/surveys"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
   --request PATCH \
   --header "Content-Type: application/json" \
---header "Authorization: Bearer ${TOKEN}" \
---data '{
-    "example": {
-      "text": "'"${TEXT}"'"
-    }
-  }'
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
+  "survey": {
+    "question": "'"${QUESTION}"'",
+    "option1": "'"${OPT1}"'",
+    "option2": "'"${OPT2}"'"
+  }
+}'
 
 echo
